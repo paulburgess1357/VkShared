28f7c112319573b54703c94ab9c1f5d257203741
Paul Burgess <74145633+paulburgess1357@users.noreply.github.com>
2022-08-15 22:20:37 -0600

    conan fix

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,4 @@
-from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
-
+from conans import ConanFile, CMake
 
 class HelloConan(ConanFile):
     name = "VkShared"
@@ -16,20 +14,13 @@ class HelloConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
     exports_sources = "CMakeLists.txt", "VkShared/*"
-    
     requires = "vulkan-loader/1.3.216.0"
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-
-    def layout(self):
-        cmake_layout(self)
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -37,8 +28,12 @@ class HelloConan(ConanFile):
         cmake.build()
 
     def package(self):
-        cmake = CMake(self)
-        cmake.install()
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["VkShared"]
