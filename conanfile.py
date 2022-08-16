489f088e20116e6376cc0e9e80844f68486c7b33
Paul Burgess <74145633+paulburgess1357@users.noreply.github.com>
2022-08-15 21:47:37 -0600

    Cmake in progress

--- conanfile.py
+++ conanfile.py
@@ -7,18 +7,15 @@ class HelloConan(ConanFile):
     version = "0.1"
 
     # Optional metadata
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Hello here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    license = "MIT"
+    author = "<Paul Burgess> <paulburgess1357@gmail.com>"
+    url = "https://github.com/paulburgess1357/VkShared"
+    description = "Vulkan Shared"
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-
-    # Sources are located in the same place as this recipe, copy them to the recipe
     exports_sources = "CMakeLists.txt", "VkShared/*"
     
     requires = "vulkan-loader/1.3.216.0"
