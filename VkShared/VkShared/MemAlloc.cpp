a3b15041d71c48b4accf609e049275a0c0597690
“Paul <paulburgess1357@gmail.com>
2022-10-29 12:26:29 -0600

    pragma update

--- VkShared/VkShared/MemAlloc.cpp
+++ VkShared/VkShared/MemAlloc.cpp
@@ -1,8 +1,9 @@
 #define VMA_IMPLEMENTATION
 #define VMA_VULKAN_VERSION 1002000
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wall"
-#pragma warning(push, 0)
-#include "vk_mem_alloc.h"
-#pragma GCC diagnostic pop
-#pragma warning(pop)
+#include "VkShared/MemAlloc.h"
+
+namespace VkShared::VMA_TEST {
+	VmaAllocation test() {
+		return VK_NULL_HANDLE;
+	}
+} // namespace VkShared::VMA_TEST
--- VkShared/VkShared/MemAlloc.h
+++ VkShared/VkShared/MemAlloc.h
@@ -1,7 +1,22 @@
 #pragma once
-#pragma warning(push, 0)
+#define VMA_VULKAN_VERSION 1002000
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wparentheses"
+
+#pragma warning(push, 0)
 #include <vk_mem_alloc.h>
-#pragma GCC diagnostic pop
 #pragma warning(pop)
+
+#pragma GCC diagnostic pop
+
+namespace VkShared::VMA_TEST {
+	[[nodiscard]] VmaAllocation test();
+} // namespace VkShared::VMA_TEST
