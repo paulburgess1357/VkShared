511876b9f56637ac3a08622bbcb19e99fd18fd18
“Paul <paulburgess1357@gmail.com>
2022-10-29 09:13:25 -0600

    misc cmake

--- VkShared/VkShared/MemAlloc.cpp
+++ VkShared/VkShared/MemAlloc.cpp
@@ -1,5 +1,7 @@
 #define VMA_IMPLEMENTATION
 #define VMA_VULKAN_VERSION 1002000
+#pragma GCC diagnostic ignored "-Wall"
 #pragma warning(push, 0)
 #include "vk_mem_alloc.h"
+#pragma GCC diagnostic pop
 #pragma warning(pop)
